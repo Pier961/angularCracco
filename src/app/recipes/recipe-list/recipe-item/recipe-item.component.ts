626ba2220324f6f12d67681e02636e0d09af0f6a
Pier <pierlu.aba@gmail.com>
2018-12-04 17:04:13 +0100

    Create list recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
-
+recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+// tslint:disable-next-line:max-line-length
+recipes: Recipe[] = [new Recipe('Patate riso e cozze', 'lorem ipmus dsa dsa', 'http://cdn.cook.stbm.it/thumbnails/ricette/1/1624/hd750x421.jpg')
+,
+// tslint:disable-next-line:max-line-length
+new Recipe('orecchiette con le cime di rapa', 'lorem ipmus dsa dsa', 'https://img2.tgcom24.mediaset.it/binary/ricetta/tgcom24/33.$plit/C_2_ricetta_1620_upiFoto.jpg')
+,
+new Recipe('bombette', 'lorem ipmus dsa dsa', 'http://www.passalorto.it/1014-large_default/spiedini-di-bombette.jpg')
+];
+constructor() { }
 
   ngOnInit() {
   }
